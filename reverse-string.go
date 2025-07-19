07cb3aa53fb153f7ac5782f78748f74e06d650b9
mfachryna <fachry.arifna@gmail.com>
2025-07-19 18:39:35 +0700

    ref: Solution 2 and 3 for  Reverse String problem

--- reverse-string.go
+++ reverse-string.go
@@ -1,9 +1,30 @@
 package relearndsa
 
+
 func reverseString(s []byte) {
 	for i := 0; i < len(s)/2; i++ {
 		temp := s[i]
 		s[i] = s[len(s)-i-1]
 		s[len(s)-i-1] = temp
 	}
+}
+
+func reverseString2(s []byte) {
+	for i := 0; i < len(s)/2; i++ {
+		s[i], s[len(s)-i-1] = s[len(s)-i-1], s[i]
+	}
+}
+
+func reverseString3(s []byte) {
+    i,l := 0, len(s)-1
+    for i < l {
+		s[i], s[l] = s[l], s[i]
+        i++
+        l--
+    }
+}
+
+
+func init() {
+    debug.SetMemoryLimit(1)
 }
