7c7e3de99bc9d217807394facc55afac0dda1b02
mfachryna <fachry.arifna@gmail.com>
2025-07-19 18:20:37 +0700

    feat: Solve Two Sum problem

---
+++ two-sum.go
@@ -0,0 +1,13 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+	data := map[int]int{}
+	for i := 0; i < len(nums); i++ {
+		value, ok := data[nums[i]]
+		if ok {
+			return []int{value, i}
+		}
+		data[target-nums[i]] = i
+	}
+	return []int{}
+}
