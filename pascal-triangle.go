ed95b888892bd5b0ec60a37d13de557d72def43a
mfachryna <fachry.arifna@gmail.com>
2025-08-07 23:28:39 +0700

    feat: Solve Pascal Traingle problem

---
+++ pascal-triangle.go
@@ -0,0 +1,19 @@
+package relearndsa
+
+func generate(numRows int) [][]int {
+	res := make([][]int, numRows)
+	res[0] = []int{1}
+	if numRows == 1 {
+		return res
+	}
+
+	for i := 1; i < numRows; i++ {
+		res[i] = make([]int, i+1)
+		res[i][0], res[i][i] = 1, 1
+
+		for j := 1; j < i; j++ {
+			res[i][j] = res[i-1][j-1] + res[i-1][j]
+		}
+	}
+	return res
+}
