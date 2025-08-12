827708ea15f711f257f24479ce79271f131ae08a
mfachryna <fachry.arifna@gmail.com>
2025-08-12 23:42:15 +0700

    feat: finish Binary Tree Postorder Traversal problems

---
+++ binary-tree-postorder-traversal.go
@@ -0,0 +1,25 @@
+package relearndsa
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func postorderTraversal(root *TreeNode) []int {
+
+	var res []int
+	var traverse func(node *TreeNode)
+	traverse = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		traverse(node.Left)
+		traverse(node.Right)
+		res = append(res, node.Val)
+	}
+	traverse(root)
+	return res
+}
